253be3509a133bfbb277e10dcd3c5f75f992b86d
Zed Yeung <shoominga@gmail.com>
2018-06-13 11:27:02 -0700

    add cronjob and test case

--- generate_random_data.sh
+++ generate_random_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-docker pull nginx
+# NOW=$(date +'%Y%m%d-%H%M%S')
+GENERATE_INTERVAL=60
 
 for i in $(seq 1 1000);
   do
     deployment=nginx${i}
     kubectl run ${deployment} --image=nginx:latest --replicas=1
-    sleep 60
+    etcdctl set /test/case${i} $(${i} * 2 - 1)
+    sleep ${GENERATE_INTERVAL}
   done
--- test.sh
+++ test.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-TEST_FULL_NUM=10
+# openssl encrypt large file
+# https://gist.github.com/crazybyte/4142975
+TEST_FULL_NUM=1
 TEST_DIFF_NUM=2
-INTERVAL=60
-SLEEP_TIME=(${TEST_FULL_NUM} + ${TEST_DIFF_NUM}) * ${INTERVAL}
-etcdctl ls /registry --recursive
+FULL_INTERVAL=600
+DIFF_INTERVAL=120
+GENERATE_INTERVAL=60
+SLEEP_TIME=$(${TEST_FULL_NUM} * ${FULL_INTERVAL} + ${TEST_DIFF_NUM} * ${DIFF_INTERVAL})
 
 openssl req -x509 -days 100000 -newkey rsa:8912 -keyout private_key.pem -out public_key.pem
-./generate_random_data.sh
+./generate_random_data.sh &
+# CRON JOB TO BACKUP
+# https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
+crontab -l > backup_cronjob
+echo "*/10 * * * * ./etcd-backup-full.sh" >> backup_cronjob
+echo "*/2 * * * * ./etcd-backup-diff.sh" >> backup_cronjob
+# echo "* * * * * ./generate_random_data.sh" >> backup_cronjob
+crontab backup_cronjob
+rm backup_cronjob
 
 sleep ${SLEEP_TIME}
 
-etcdctl rm /registry --recursive
+# simulate clash
+etcdctl rm / --recursive
+# TODO SEND ALERT
 
 LATEST_FULL_BACKUP=latest_full_backup.json
 LATEST_DIFF_BACKUP=latest_diff_backup.patch
@@ -20,21 +33,27 @@ openssl smime -decrypt -binary -in ${LATEST_FULL_BACKUP}.enc -inform DER -out ${
 openssl smime -decrypt -binary -in ${LATEST_DIFF_BACKUP}.enc -inform DER -out ${LATEST_DIFF_BACKUP} -inkey private_key.pem
 
 # TEST FULL BACKUP
-etcdtool --ca ./etcdcert.crt --peers host -u root import -y /registry ${LATEST_FULL_BACKUP}
-for i in $(seq 1 ${TEST_FULL_NUM});
+etcdtool --ca ./etcdcert.crt --peers host -u root import -y / ${LATEST_FULL_BACKUP}
+
+FULL_BACKUP_TEST_CASE_NUM=$( ${TEST_FULL_NUM} * ${FULL_INTERVAL} / ${GENERATE_INTERVAL} )
+assert $(etcdctl ls /test | wc -l) ${FULL_BACKUP_TEST_CASE_NUM}
+
+for i in $(seq 1 ${FULL_BACKUP_TEST_CASE_NUM});
   do
-    deployment=nginx${i}
-    value=$(etcdctl get /registry/deployments/${deployment})
-    assert ${value}
+    # deployment=nginx${i}
+    # assert $(etcdctl get /registry/deployments/${deployment})
+    assert $(etcdctl get /test/case${i}) $(${i} * 2 - 1)
+
 
 # TEST DIFF BACKUP
 UPDATED_BACKUP=updated_backup.json
 patch ${LATEST_FULL_BACKUP} -i ${LATEST_DIFF_BACKUP} -o ${UPDATED_BACKUP}
 
-etcdtool --ca ./etcdcert.crt --peers host -u root import -y /registry ${UPDATED_BACKUP}
+etcdtool --ca ./etcdcert.crt --peers host -u root import -y / ${UPDATED_BACKUP}
 
-for i in $(seq 1 (${TEST_FULL_NUM} + ${TEST_DIFF_NUM}));
+DIFF_BACKUP_TEST_CASE_NUM=$(${SLEEP_TIME} / ${GENERATE_INTERVAL} )
+assert $(etcdctl ls /test | wc -l) ${DIFF_BACKUP_TEST_CASE_NUM}
+
+for i in $(seq 1 ${DIFF_BACKUP_TEST_CASE_NUM});
   do
-    deployment=nginx${i}
-    value=$(etcdctl get /registry/deployments/${deployment})
-    assert ${value}
+    assert $(etcdctl get /test/case${i}) $(${i} * 2 - 1)
