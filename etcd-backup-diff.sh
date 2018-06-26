e3a84229b1f174ca1fda9e2ba44787fbaf6dab11
Zed Yeung <shoominga@gmail.com>
2018-06-26 12:33:33 -0700

    add s3cmd config

--- etcd-backup-diff.sh
+++ etcd-backup-diff.sh
@@ -9,6 +9,7 @@ NOW=$(date +'%Y%m%d-%H%M%S')
 FULL_BACKUP_DIR=/etcd_backup/full/
 DIFF_BACKUP_DIR=/etcd_backup/diff/
 DIFF_BACKUP_OBJECT_STORAGE_BUCKET=s3://diff-backup
+DIFF_BACKUP_S3CFG=~/.diff-s3cfg
 LATEST_FULL_BACKUP=($( ls -tp ${FULL_BACKUP_DIR} | head -n 1))
 UPDATED_FULL_BACKUP=${NOW}.json
 DIFF_BACKUP=${NOW}.patch
@@ -22,7 +23,7 @@ mkdir -p ${DIFF_BACKUP_DIR}
 etcdtool --peers ${HOSTS} -u ${USER} export ${BACKUP_ENDPOINT} -f 'JSON' -o ${UPDATED_FULL_BACKUP}
 openssl smime -encrypt -binary -aes-256-cbc -in ${DIFF_BACKUP} -out ${DIFF_BACKUP}.enc -outform DER ${PUBLIC_KEY_PEM}
 diff ${LATEST_FULL_BACKUP} ${UPDATED_FULL_BACKUP} > ${DIFF_BACKUP}
-s3cmd put ${DIFF_BACKUP}.enc ${DIFF_BACKUP_OBJECT_STORAGE_BUCKET}/${DIFF_BACKUP}.enc
+s3cmd -c ${DIFF_BACKUP_S3CFG} put ${DIFF_BACKUP}.enc ${DIFF_BACKUP_OBJECT_STORAGE_BUCKET}/${DIFF_BACKUP}.enc
 
 # REMOVE OUTDATED BACKUP
 DIFF_BACKUP_NUM=$(ls -l ${DIFF_BACKUP_DIR} | wc -l)
--- etcd-backup-full.sh
+++ etcd-backup-full.sh
@@ -8,6 +8,7 @@ HOSTS=(
 NOW=$(date +'%Y%m%d-%H%M%S')
 FULL_BACKUP_DIR=/etcd_backup/full/
 FULL_BACKUP_OBJECT_STORAGE_BUCKET=s3://full-backup
+FULL_BACKUP_S3CFG=~/.full-s3cfg
 FULL_BACKUP=${NOW}.json
 BACKUP_ENDPOINT=/
 RETAIN=1
@@ -19,7 +20,7 @@ mkdir -p ${FULL_BACKUP_DIR}
 # https://gist.github.com/crazybyte/4142975
 etcdtool --peers ${HOSTS} -u ${USER} export ${BACKUP_ENDPOINT} -f 'JSON' -o ${FULL_BACKUP_DIR}/${FULL_BACKUP}
 openssl smime -encrypt -binary -aes-256-cbc -in ${FULL_BACKUP} -out ${FULL_BACKUP}.enc -outform DER ${PUBLIC_KEY_PEM}
-s3cmd put ${FULL_BACKUP}.enc ${FULL_BACKUP_OBJECT_STORAGE_BUCKET}/${FULL_BACKUP}.enc
+s3cmd -c ${FULL_BACKUP_S3CFG} put ${FULL_BACKUP}.enc ${FULL_BACKUP_OBJECT_STORAGE_BUCKET}/${FULL_BACKUP}.enc
 
 # REMOVE OUTDATED BACKUP
 FULL_BACKUP_NUM=$(ls -l ${FULL_BACKUP_DIR} | wc -l)
