ae0ea3ed8a2158d39564765d4e068d9d2101ca2b
Zed Yeung <shoominga@gmail.com>
2018-06-27 10:30:57 -0700

    modify s3cmd configure

--- etcd-backup-full.sh
+++ etcd-backup-full.sh
@@ -4,19 +4,17 @@ ENDPOINTS="http://10.50.216.13:4001,http://10.73.146.15:4001,http://10.92.215.12
 NOW=$(date +'%Y%m%d-%H%M%S')
 FULL_BACKUP_DIR=/etcd_backup/full/
 FULL_BACKUP_OBJECT_STORAGE_BUCKET=s3://full-backup
-FULL_BACKUP_S3CFG=~/.full-s3cfg
 FULL_BACKUP=${NOW}.json
 BACKUP_ENDPOINT=/
 RETAIN=1
 PUBLIC_KEY_PEM=public_key.pem
-USER=root
 
 mkdir -p ${FULL_BACKUP_DIR}
 
 # https://gist.github.com/crazybyte/4142975
 etcdtool --peers ${ENDPOINTS} export ${BACKUP_ENDPOINT} -f 'JSON' -o ${FULL_BACKUP_DIR}/${FULL_BACKUP}
 openssl smime -encrypt -binary -aes-256-cbc -in ${FULL_BACKUP} -out ${FULL_BACKUP}.enc -outform DER ${PUBLIC_KEY_PEM}
-s3cmd -c ${FULL_BACKUP_S3CFG} put ${FULL_BACKUP}.enc ${FULL_BACKUP_OBJECT_STORAGE_BUCKET}/${FULL_BACKUP}.enc
+s3cmd put ${FULL_BACKUP}.enc ${FULL_BACKUP_OBJECT_STORAGE_BUCKET}/${FULL_BACKUP}.enc
 
 # REMOVE OUTDATED BACKUP
 FULL_BACKUP_NUM=$(ls -l ${FULL_BACKUP_DIR} | wc -l)
