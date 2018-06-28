c7725df58af9337fc30040f208fd2175529c6ba6
Zed Yeung <shoominga@gmail.com>
2018-06-28 14:22:42 -0700

    add cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+RESTORE_HOST0=10.103.1.13
+RESTORE_HOST1=10.103.1.14
+RESTORE_HOST2=10.103.1.15
+HOST0=10.103.1.16
+HOST1=10.103.1.17
+HOST2=10.103.1.18
+PORT=2379
+
+RESTORE_ENDPOINTS="http://${RESTORE_HOST0}:${PORT},http://${RESTORE_HOST1}:${PORT},http://${RESTORE_HOST2}:${PORT}"
+ENDPOINTS="http://${HOST0}:${PORT},http://${HOST1}:${PORT},http://${HOST2}:${PORT}"
+
 crontab -r
 
-etcdctl rm -r /
+etcdctl --endpoints ${ENDPOINTS} rm -r /test
+etcdctl --endpoints ${RESTORE_ENDPOINTS} rm -r /tesst
 
-s3cmd rm s3://full-backup --recursive
-s3cmd rm s3://diff-backup --recursive
+s3cmd rm s3://full-backup/*
+s3cmd rm s3://diff-backup/*
+
+rm -r /etcd_backup/full/
+rm -r /etcd_backup/diff/
