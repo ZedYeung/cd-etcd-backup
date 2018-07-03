606a75af9be709c52141db9190094e528689660e
Zed Yeung <shoominga@gmail.com>
2018-07-03 10:00:20 -0700

    refactor: test.sh

--- test.sh
+++ test.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
 # openssl encrypt large file
 # https://gist.github.com/crazybyte/4142975
-RESTORE_HOST0=10.103.1.13
-RESTORE_HOST1=10.103.1.14
-RESTORE_HOST2=10.103.1.15
-HOST0=10.103.1.16
-HOST1=10.103.1.17
-HOST2=10.103.1.18
-PORT=2379
+echo "Setup environment variable"
+./set_env_variable.sh
 
-export SLACK_APP="https://hooks.slack.com/services/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"
-export RESTORE_ENDPOINTS="http://${RESTORE_HOST0}:${PORT},http://${RESTORE_HOST1}:${PORT},http://${RESTORE_HOST2}:${PORT}"
-export ENDPOINTS="http://${HOST0}:${PORT},http://${HOST1}:${PORT},http://${HOST2}:${PORT}"
+# used in generate_random_data.sh
 export GENERATE_INTERVAL=10
 
 TEST_FULL_NUM=2
-TEST_DIFF_NUM=2
+TEST_NUM=2
 FULL_INTERVAL=180
-DIFF_INTERVAL=60
-CUSHION_INTERVAL=15
+INTERVAL=60
+CUSHION_INTERVAL=10
 
-FULL_BACKUP_OBJECT_STORAGE_BUCKET=s3://full-backup
-DIFF_BACKUP_OBJECT_STORAGE_BUCKET=s3://diff-backup
-BACKUP_ENDPOINT=/
-PRIVATE_KEY_PEM=private_key.pem
-PUBLIC_KEY_PEM=public_key.pem
-SLEEP_TIME=$[${TEST_FULL_NUM} * ${FULL_INTERVAL} + ${TEST_DIFF_NUM} * ${DIFF_INTERVAL} + ${CUSHION_INTERVAL}]
+SLEEP_TIME=$[${TEST_FULL_NUM} * ${FULL_INTERVAL} + ${TEST_NUM} * ${INTERVAL} + ${CUSHION_INTERVAL}]
 
 echo "Generate ssl file..."
 openssl req -x509 -days 100000 -newkey rsa:4096 -keyout ${PRIVATE_KEY_PEM} -out ${PUBLIC_KEY_PEM}
 
 echo "Generate data..."
 ./generate_random_data.sh &
-
-echo "First backup"
-./etcd-backup-full.sh
-./etcd-backup-diff.sh
 
 # CRON JOB TO BACKUP
 # https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
@@ -51,62 +35,16 @@ sleep ${SLEEP_TIME}
 # etcdctl rm ${BACKUP_ENDPOINT} --recursive
 # TODO VPN CONNECT
 echo "Restore..."
-# s3cmd output sample
-# 2018-06-28 22:47      3576   s3://full-backup/test.sh
-LATEST_FULL_ENC_BACKUP=$(basename $(s3cmd ls ${FULL_BACKUP_OBJECT_STORAGE_BUCKET} | tail -n 1 | awk {'print $4'}))
-LATEST_DIFF_ENC_BACKUP=$(basename $(s3cmd ls ${DIFF_BACKUP_OBJECT_STORAGE_BUCKET} | tail -n 1 | awk {'print $4'}))
-# LATEST_FULL_BACKUP=$(${LATEST_FULL_ENC_BACKUP} | rev | cut -f 2- -d '.' | rev)
-LATEST_FULL_BACKUP=$(basename ${LATEST_FULL_ENC_BACKUP} .enc)
-echo ${LATEST_FULL_BACKUP}
-# LATEST_DIFF_BACKUP=$(${LATEST_DIFF_ENC_BACKUP} | rev | cut -f 2- -d '.' | rev)
-LATEST_DIFF_BACKUP=$(basename ${LATEST_DIFF_ENC_BACKUP} .enc)
-echo ${LATEST_DIFF_BACKUP}
+./restore.sh
 
-echo "Pulling ${LATEST_FULL_ENC_BACKUP}"
-s3cmd get ${FULL_BACKUP_OBJECT_STORAGE_BUCKET}/${LATEST_FULL_ENC_BACKUP} ${LATEST_FULL_ENC_BACKUP}
-echo "Pulling ${LATEST_DIFF_ENC_BACKUP}"
-s3cmd get ${DIFF_BACKUP_OBJECT_STORAGE_BUCKET}/${LATEST_DIFF_ENC_BACKUP} ${LATEST_DIFF_ENC_BACKUP}
-
-echo "Descrypting..."
-openssl smime -decrypt -binary -in ${LATEST_FULL_ENC_BACKUP} -inform DER -out ${LATEST_FULL_BACKUP} -inkey ${PRIVATE_KEY_PEM}
-openssl smime -decrypt -binary -in ${LATEST_DIFF_ENC_BACKUP} -inform DER -out ${LATEST_DIFF_BACKUP} -inkey ${PRIVATE_KEY_PEM}
-
-# TEST FULL BACKUP
-# TODO: CA
-echo "Recovering..."
-etcdtool --peers ${RESTORE_ENDPOINTS} import -y ${BACKUP_ENDPOINT} ${LATEST_FULL_BACKUP}
-
-FULL_BACKUP_TEST_CASE_NUM=$[${TEST_FULL_NUM} * ${FULL_INTERVAL} / ${GENERATE_INTERVAL}]
-RESTORE_FULL_BACKUP_NUM=$(etcdctl --endpoints ${RESTORE_ENDPOINTS} ls /test | wc -l)
-if  [ ${RESTORE_FULL_BACKUP_NUM} -ne ${FULL_BACKUP_TEST_CASE_NUM} ]; then
-  echo "Full backup test case number: ${FULL_BACKUP_TEST_CASE_NUM}"
-  echo "Restore full backup number: ${RESTORE_FULL_BACKUP_NUM}"
+BACKUP_TEST_CASE_NUM=$[ (${TEST_FULL_NUM} * ${FULL_INTERVAL} + ${TEST_NUM} * ${INTERVAL}) / ${GENERATE_INTERVAL}]
+RESTORE_BACKUP_NUM=$(etcdctl --endpoints ${RESTORE_ENDPOINTS} ls /test | wc -l)
+if  [ ${RESTORE_BACKUP_NUM} -ne ${BACKUP_TEST_CASE_NUM} ]; then
+  echo "Backup test case number: ${BACKUP_TEST_CASE_NUM}"
+  echo "Restore backup test case number: ${RESTORE_BACKUP_NUM}"
 fi
 
-for i in $(seq 1 ${FULL_BACKUP_TEST_CASE_NUM});
-do
-  # deployment=nginx${i}
-  # assert $(etcdctl get /registry/deployments/${deployment})
-  if [ "$(etcdctl --endpoints ${RESTORE_ENDPOINTS} get /test/case${i})" -ne $[${i} * 2 - 1] ]; then
-    echo "mismatch"
-  fi
-done
-
-
-# TEST DIFF BACKUP
-UPDATED_FULL_BACKUP=updated_full_backup.json
-patch ${LATEST_FULL_BACKUP} -i ${LATEST_DIFF_BACKUP} -o ${UPDATED_FULL_BACKUP}
-
-etcdtool --peers ${RESTORE_ENDPOINTS} import -y ${BACKUP_ENDPOINT} ${UPDATED_FULL_BACKUP}
-
-DIFF_BACKUP_TEST_CASE_NUM=$[ (${TEST_FULL_NUM} * ${FULL_INTERVAL} + ${TEST_DIFF_NUM} * ${DIFF_INTERVAL}) / ${GENERATE_INTERVAL}]
-RESTORE_DIFF_BACKUP_NUM=$(etcdctl --endpoints ${RESTORE_ENDPOINTS} ls /test | wc -l)
-if  [ ${RESTORE_DIFF_BACKUP_NUM} -ne ${DIFF_BACKUP_TEST_CASE_NUM} ]; then
-  echo "Diff backup test case number: ${DIFF_BACKUP_TEST_CASE_NUM}"
-  echo "Restore diff backup number: ${RESTORE_DIFF_BACKUP_NUM}"
-fi
-
-for i in $(seq 1 ${DIFF_BACKUP_TEST_CASE_NUM});
+for i in $(seq 1 ${BACKUP_TEST_CASE_NUM});
 do
   if [ "$(etcdctl --endpoints ${RESTORE_ENDPOINTS} get /test/case${i})" -ne $[${i} * 2 - 1] ]; then
     echo "mismatch"
